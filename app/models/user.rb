1648de8880fb55df6b1f6ae919679f124321ffbf
sryuri <yuri.tayama@gmail.com>
2021-10-17 14:06:57 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname , presence: true
-  validates :family_name , presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name , presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday , presence: true
-    
-  
 
 end
