f5e40e97311b9925756e4382e85107b86533eae5
sryuri <yuri.tayama@gmail.com>
2021-10-18 22:44:51 +0900

    ActiveHashアソシエーションの設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,6 +12,10 @@ class category < ActiveHash::Base
       { id: 10, name: 'ハンドメイド' },
       { id: 11, name: 'その他' }
     ]
+
+    include ActiveHash::Associations
+    has_many :items
+
 end
 
     
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+
     belongs_to :user 
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :shipping_cost
+    belongs_to :shipping_area
+    belongs_to :shipping_day
 end
