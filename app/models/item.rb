33b97f45b247233023778769eb67d817f6b348e3
sryuri <yuri.tayama@gmail.com>
2021-10-19 08:23:56 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,22 @@
 class Item < ApplicationRecord
 
     belongs_to :user 
+
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
     belongs_to :condition
     belongs_to :shipping_cost
     belongs_to :shipping_area
     belongs_to :shipping_day
+
+    validates :name, presence: true
+    validates :text, presence: true
+    validates :price, presence: true
+    validates :image, presence: true
+    validates :category_id, numericality: { other_than: 1, , message: "can't be blank" } 
+    validates :condition_id, numericality: { other_than: 1, , message: "can't be blank" } 
+    validates :shipping_cost_id, numericality: { other_than: 1, , message: "can't be blank" } 
+    validates :shipping_area_id, numericality: { other_than: 1, , message: "can't be blank" }
+    validates :shipping_day_id, numericality: { other_than: 1, , message: "can't be blank" }
+
 end
