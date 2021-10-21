11b430d1ee4e74900949e2edb328fcefa859dbfa
sryuri <yuri.tayama@gmail.com>
2021-10-21 21:50:21 +0900

    コントローラーの設定

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,10 +1,24 @@
 class BuysController < ApplicationController
 
     def index
-        @buy_address = class BuyAddress.new
+        @buy_address = BuyAddress.new
     end
 
     def create
- 
+        @buy_address  = BuyAddress.new(buy_address_params)
+        if @buy_address.valid?
+          @buy_address.save
+          redirect_to root_path
+        else
+          render "buys/index"
+        end
     end   
+
+
+    private
+
+    def buy_address_params
+      params.require(buy_address).permit(:zip_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :item_id, :buy_id).merge(user_id: current_user.id)
+    end
+
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -3,18 +3,18 @@ class BuyAddress
     attr_accessor :buy_id, :zip_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :item_id, :user_id
 
     validates :buy_id, presence: true
-    validates :zip_code, presence: true, format: { with: \A\d{3}[-]?\d{4}\z }
+    validates :zip_code, presence: true, format: { with: /\A\d{3}[-]?\d{4}\z/ }
     validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city, presence: true
     validates :address, presence: true
-    validates :building_name
-    validates :phone_number, presence: true, format: { with: /^\d{10}$|^\d{11}$/ }
+    validate  :building_name
+    validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
     validates :item_id, presence: true
     validates :user_id, presence: true
 
     def save   
         buy = Buy.create(item_id: item_id, user_id: user_id)
-        Address.create(buy_id: buy_id, zip_code: zip_code, shipping_area_id: shipping_area_id,city: city, address: address, building_name: building_name, phone_number: phone_number)
+        Address.create(buy_id: buy_id, zip_code: zip_code, shipping_area_id: shipping_area_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
     end
 
 end
