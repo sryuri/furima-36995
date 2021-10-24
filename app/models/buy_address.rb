eeb3390b64313c4889e89a5f408414961ecb8036
sryuri <yuri.tayama@gmail.com>
2021-10-24 14:34:55 +0900

    with_optionsの設定

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -2,14 +2,16 @@ class BuyAddress
     include ActiveModel::Model
     attr_accessor :zip_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :item_id, :user_id, :token
     
-    validates :zip_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city, presence: true
-    validates :address, presence: true
-    validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
-    validates :item_id, presence: true
-    validates :user_id, presence: true
-    validates :token, presence: true
+    with_options presence: true do
+        validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+        validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
+        validates :city
+        validates :address
+        validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+        validates :item_id
+        validates :user_id
+        validates :token
+    end
 
     def save   
         buy = Buy.create(item_id: item_id, user_id: user_id)
